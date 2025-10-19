0dcbe195c74b5b75cda849b8e56b1d4732f57a5e
wijdan-ali <wijdan.nadeem@gmail.com>
2025-10-19 13:28:51 -0400

    Update decimal_st_ui.py

--- decimal_st_ui.py
+++ decimal_st_ui.py
@@ -27,7 +27,7 @@ decimal_year = year + (days_passed / days_in_year)
 # --- Output ---
 st.markdown(f""" 
 
-<div style="text-align: center; display: flex; align-items: center; justify-content: center;"><span style="font-size: 1.3em; font-weight: bold;">Decimal Year: </span> <h2>{decimal_year:.6f}</h2>
+<div style="text-align: center; display: flex; align-items: center; justify-content: center;"><span style="font-size: 1.3em; font-weight: 400;">Decimal Year:    </span> <h2>{decimal_year:.6f}</h2>
 </div>
 
 
